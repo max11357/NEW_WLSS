a19268af03927c9e1a1475d006b160019d0a9911
mac <max7456@gmail.com>
2018-08-28 21:28:46 +0700

    ทำ l_3

--- l_3.py
+++ l_3.py
@@ -22,7 +22,7 @@ def random_nodes(width, height, station, set_energy, density):
         random_x, random_y = rd.randint(0, width), rd.randint(0, height)
         if [random_x, random_y] not in node_member and \
            [random_x, random_y] not in station:
-            node_member.append([random_x, random_y, set_energy])
+            node_member.append([count, random_x, random_y, set_energy])
         count += 1
     return node_member, len_nodes
 
@@ -38,17 +38,46 @@ def random_cch(node_member, t_predefine, len_nodes):
         cch.append(c_cluster)
         node_member.remove(c_cluster)
         count += 1
-    return cch
+    return cch, node_member
 
 
-def distance_candidate(cch, pkt_control, elec_tran, elec_rec, fs, mpf, d_threshold, r1):
+def distance_candidate(node_member, cch, pkt_control, elec_tran, elec_rec, fs, mpf, d_threshold, r1):
     cluster_member = []
+    cch.sort()
     print("cch : "+ str(len(cch)))
     for main in cch:
+        check_dis = []
+        most_energy =  main # deafult is Receiver
         for other in cch:
-            distance = math.sqrt((main[0] - other[0])**2 + (main[1] - other[1])**2)
-            print(distance)
+            distance = math.sqrt((main[1] - other[1])**2 + (main[2] - other[2])**2)
+            check_dis.append([other[0],distance])
+            # Send pkt control
+            if  distance < d_threshold:
+                main[3] = main[3] - ((elec_tran+(fs*(distance**2)))*pkt_control)
+            elif distance >= d_threshold :
+                main[3] = main[3] - ((elec_tran+(mpf*(distance**4)))*pkt_control)
+            # Receive pkt control
+            other[3] = other[3] - (elec_rec*pkt_control)
+
+        for i in range(len(check_dis)):
+            print(check_dis[i])
+        print(str(most_energy) + "  BEFORE")
+        print("--")
+
+        for i in range(len(check_dis)):
+            # if check_dis[i][1] < 60 and check_dis[i][0] != most_energy[0]:
+            #     print(str(most_energy[3]) + " & " + str(cch[i][3]))
+            if check_dis[i][1] < 60 and check_dis[i][0] != most_energy[0] and cch[i][3] > most_energy[3]:
+                most_energy = cch[i]
+            elif check_dis[i][1] < 60 and check_dis[i][0] != most_energy[0] and cch[i][3] <= most_energy[3]:
+                node_member.append(cch[i])
+                cch.remove(cch[i])
+        cluster_member.append(most_energy)
+        cch.remove(most_energy)
+        print(str(most_energy) + "  AFTER")
         print("*********************************")
+
+        
 
 
 def plot_graph(cluster_member, node_member, cch, station):
@@ -90,11 +119,11 @@ def start():
     node_member, len_nodes = \
         random_nodes(width, height, station, set_energy, density)
 
-    cch = \
+    cch, node_member = \
         random_cch(node_member, t_predefine, len_nodes)
 
 
-    distance_candidate(cch, pkt_control, elec_tran, elec_rec, fs, mpf, d_threshold, r1)
+    distance_candidate(node_member, cch, pkt_control, elec_tran, elec_rec, fs, mpf, d_threshold, r1)
 
     # plot_graph(cluster_member, node_member, cch, station)
 
