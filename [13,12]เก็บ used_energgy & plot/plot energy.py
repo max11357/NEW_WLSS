e4f9f6baba302fde37e1603d5a81d675338acb6a
jjanee <58070019@kmitl.ac.th>
2018-12-15 03:27:28 +0700

    update plot energy

--- [13,12]เก็บ used_energgy & plot/plot energy.py
+++ [13,12]เก็บ used_energgy & plot/plot energy.py
@@ -104,6 +104,8 @@ def count_lap():
             ch_bs += float(item[7])
             ch_t += float(item[8])
             cm_t += float(item[9])
+
+            
         eval('data_dyn_%d'% (i)).append([cch_sent, cch_recive, ch_sent, ch_recive, cm_send, cm_recive, ch_merg, ch_bs, ch_t, cm_t])
         cch_sent, cch_recive, ch_sent, ch_recive = 0,0,0,0
         cm_send, cm_recive, ch_merg, ch_bs, ch_t, cm_t = 0,0,0,0,0, 0
@@ -114,40 +116,114 @@ def count_lap():
               'cm_send', 'cm_recive', 'ch_merg', 'ch_bs', 'ch_t' , 'cm_t')
         
         for x in range(10):
+            
             data = eval('data_dyn_%d'% (i))[0][x]/sum(eval('data_dyn_%d'% (i))[0])*100
             eval('per_dyn_%d'% (i)).append(data)
-            
         plt.bar(xi2, eval('per_dyn_%d'% (i)))
         plt.ylim(0,30)
         plt.savefig('used_energy dyn'+str(i/10)+'.png')
         plt.close()
 
-        for mem in range(1,10):
-            print(mem)
-            eval('c%d'% (mem)).append((eval('per_dyn_%d'% (i))[mem]))
+    
+        for val in range(1,10):
+            for index in eval('per_dyn_%d'% (val)):
+                eval('c%d'%(val)).append(index)
 
-        for fmem in range(1,8):
-            eval('f%d'% (fmem)).append((eval('per_fix_%d'% (i))[fmem]))
-
+        for val in range(1,10):
+            for index in eval('per_fix_%d'% (val)):
+                eval('f%d'%(val)).append(index)
+                
     a = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 ]
     ai = [ i for i in a]
 
     
     plt.subplot(121)
-    for n in range(1,8):
-        plt.plot(ai, eval('f%d'% (n)), marker='o', label=str(xi[n]))
-        plt.legend()
-        plt.title('fix')
-        plt.ylim(-2,35)
+    keep1,keep2,keep3,keep4,keep5 = [],[],[],[],[]
+    keep6,keep7,keep8,keep9 = [],[],[],[]
+    dkeep1,dkeep2,dkeep3,dkeep4,dkeep5 = [],[],[],[],[]
+    dkeep6,dkeep7,dkeep8,dkeep9,dkeep10 = [],[],[],[],[]
 
+    for n in range(1,10):
+        keep1.append(eval('f%d'% (n))[0])
+        keep2.append(eval('f%d'% (n))[1])
+        keep3.append(eval('f%d'% (n))[2])
+        keep4.append(eval('f%d'% (n))[3])
+        keep5.append(eval('f%d'% (n))[4])
+        keep6.append(eval('f%d'% (n))[5])
+        keep7.append(eval('f%d'% (n))[6])
+        keep8.append(eval('f%d'% (n))[7])
+
+        dkeep1.append(eval('c%d'% (n))[0])
+        dkeep2.append(eval('c%d'% (n))[1])
+        dkeep3.append(eval('c%d'% (n))[2])
+        dkeep4.append(eval('c%d'% (n))[3])
+        dkeep5.append(eval('c%d'% (n))[4])
+        dkeep6.append(eval('c%d'% (n))[5])
+        dkeep7.append(eval('c%d'% (n))[6])
+        dkeep8.append(eval('c%d'% (n))[7])
+        dkeep9.append(eval('c%d'% (n))[8])
+        dkeep10.append(eval('c%d'% (n))[9])
+##        keep9.append(eval('f%d'% (n))[8])
+        
+    plt.plot(ai, keep1, marker='o', label= 'CCH anouncement')
+    plt.plot(ai, keep2, marker='o', label= 'cch recive ')
+    plt.plot(ai, keep3, marker='o', label= 'CH anouncement')
+    plt.plot(ai, keep4, marker='o', label= 'CH recive ')
+    plt.plot(ai, keep5, marker='o', label= 'CM send data')
+    plt.plot(ai, keep6, marker='o', label= 'CM recive data')
+    plt.plot(ai, keep7, marker='o', label= 'CH mearge ')
+    plt.plot(ai, keep8, marker='o', label= 'CH sent to bs')
+    plt.ylabel('Percent')
+    plt.xlabel('Fix T Predefine')
+    plt.ylim(-2,40)
+    plt.title('Used Energy for Fix T Predefine')
+    plt.legend()
 
     plt.subplot(122)
-    for n in range(1,10):
-        plt.title('dynamic')
-        plt.plot(ai, eval('c%d'% (n)), marker='o', label=str(xi2[n]))
-        plt.legend()
-        plt.ylim(-2,35)
+    plt.plot(ai, dkeep1, marker='o', label= 'CCH anouncement')
+    plt.plot(ai, dkeep2, marker='o', label= 'cch recive ')
+    plt.plot(ai, dkeep3, marker='o', label= 'CH anouncement')
+    plt.plot(ai, dkeep4, marker='o', label= 'CH recive ')
+    plt.plot(ai, dkeep5, marker='o', label= 'CM send data')
+    plt.plot(ai, dkeep6, marker='o', label= 'CM recive data')
+    plt.plot(ai, dkeep7, marker='o', label= 'CH mearge ')
+    plt.plot(ai, dkeep8, marker='o', label= 'CH sent to bs')
+    plt.plot(ai, dkeep9, marker='o', label= 'CH Change T ')
+    plt.plot(ai, dkeep10, marker='o', label= 'CM recive T')
+    plt.ylabel('Percent')
+    plt.xlabel('Dynamic T Predefine')
+    plt.ylim(-2,40)
+    plt.title('Used Energy for Fix T Dynamic')
+    plt.legend()
     plt.show()
+
+    plt.subplot(121)
+    plt.plot(ai, keep1, marker='o', label= ' Fix CCH anouncement')
+    plt.plot(ai, dkeep1, marker='o', label= 'Dynamic CCH anouncement')
+    plt.ylim(-2,30)
+    plt.ylabel('Percent')
+    plt.xlabel('T Predefine')
+    plt.title('Used Energy for send CCH anouncement')
+    plt.legend()
+
+##    plt.subplot(122)
+##    plt.plot(ai, kdeep1, marker='o', label= 'CCH anouncement')
+##    plt.ylim(-2,40)
+##    plt.ylabel('Percent')
+##    plt.xlabel('Dynamic T Predefine')
+##    plt.title('Used Energy for Dynamic T Predefine')
+##    plt.legend()
+    plt.show()
+    
+##    for b in range(1, 10):
+##        eval('keep'%(b)).append(eval('f%d'% (b))[b])
+##    plt.plot(ai, keep1, marker='o')
+##    plt.show()
+##            plt.legend()
+##            plt.title('fix')
+##            plt.ylim(-2,35)
+        
+        
 
     return data_fix_1, data_fix_2, data_fix_3, data_fix_4, data_fix_5,\
            data_fix_6, data_fix_7, data_fix_8, data_fix_9, \
