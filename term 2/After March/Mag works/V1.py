6701c79c118be630947f665f1e684f41be45ab6b
maxeskdee <max7456@gmail.com>
2019-05-08 18:13:36 +0700

    Update V1.py

--- term 2/After March/Mag works/V1.py
+++ term 2/After March/Mag works/V1.py
@@ -632,13 +632,7 @@ def optimize_t(cluster_head, cluster_member, cm_select, max_distance, decimal, \
                         elif cluster_member[cm][2] <= e_avg*0.95 and cluster_member[cm][2] > e_avg*0.9:
                             cluster_member[cm][3] =  round(cluster_member[cm][3] - 0.0075, decimal)
                         elif cluster_member[cm][2] <= e_avg*0.9:
-                            cluster_member[cm][3] =  round(cluster_member[cm][3] - increase_t, decimal)
-
-        for k in cluster_head:
-            print(k[3])
-        for p in cluster_member:
-            print(p[3])
-        print("-------------")        
+                            cluster_member[cm][3] =  round(cluster_member[cm][3] - increase_t, decimal)       
 
 
     return cluster_head, cluster_member, dead, ch_t_compare, check_optimize_t
